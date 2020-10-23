923ad11fec3fe28a8a82170571685f334b0fd6c6
jehnls <jeferson_nls@hotmail.com>
2020-10-23 15:24:21 -0300

    If applied this commit will, delete folder node_modules, add file gitignore, and edit package.json

--- src/pages/Orphanage.tsx
+++ src/pages/Orphanage.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { FaWhatsapp } from "react-icons/fa";
 import { FiClock, FiInfo } from "react-icons/fi";
 import { Map, Marker, TileLayer } from "react-leaflet";
 import { useParams } from 'react-router-dom'
@@ -33,41 +32,41 @@ export default function Orphanage() {
   const [orphanage, setOrphanage] = useState<Orphanage>();
   const [activeImageIndex, setActiveImageIndex] = useState(0);
 
-    useEffect(() => {
-        api.get(`orphanages/${params.id}`).then(response => {
-            setOrphanage(response.data);
-        })
-    }, [params.id]);
+  useEffect(() => {
+    api.get(`orphanages/${params.id}`).then(response => {
+      setOrphanage(response.data);
+    })
+  }, [params.id]);
 
-    if(!orphanage) {
-      return <p>Carregando...</p>
-    }
+  if (!orphanage) {
+    return <p>Carregando...</p>
+  }
 
   return (
     <div id="page-orphanage">
       <Sidebar />
-      
+
 
       <main>
         <div className="orphanage-details">
           <img src={orphanage.images[activeImageIndex].url} alt={orphanage.name} />
 
           <div className="images">
-            {orphanage.images.map((image, index) =>  {
-              return(           
-                 <button 
-                 key={image.id} 
-                 className={activeImageIndex === index ? 'active' : ''} 
-                 type="button"
-                 onClick={() =>{
-                   setActiveImageIndex(index)
+            {orphanage.images.map((image, index) => {
+              return (
+                <button
+                  key={image.id}
+                  className={activeImageIndex === index ? 'active' : ''}
+                  type="button"
+                  onClick={() => {
+                    setActiveImageIndex(index)
                   }}>
-              <img src={image.url} alt={orphanage.name} />
-            </button>
+                  <img src={image.url} alt={orphanage.name} />
+                </button>
               )
             })}
           </div>
-          
+
           <div className="orphanage-details-content">
             <h1>{orphanage.name}</h1>
             <p>
@@ -75,9 +74,9 @@ export default function Orphanage() {
             </p>
 
             <div className="map-container">
-              <Map 
-                center={[orphanage.latitude, orphanage.longitude]} 
-                zoom={16} 
+              <Map
+                center={[orphanage.latitude, orphanage.longitude]}
+                zoom={16}
                 style={{ width: '100%', height: 280 }}
                 dragging={false}
                 touchZoom={false}
@@ -85,7 +84,7 @@ export default function Orphanage() {
                 scrollWheelZoom={false}
                 doubleClickZoom={false}
               >
-                <TileLayer 
+                <TileLayer
                   url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
                 />
                 <Marker interactive={false} icon={MapIcon} position={[orphanage.latitude, orphanage.longitude]} />
@@ -109,19 +108,19 @@ export default function Orphanage() {
                 Segunda à Sexta <br />
                 {orphanage.opening_hours}
               </div>
-              { orphanage.open_on_weekends ? (
+              {orphanage.open_on_weekends ? (
                 <div className="open-on-weekends">
-                <FiInfo size={32} color="#39CC83" />
+                  <FiInfo size={32} color="#39CC83" />
                 Atendemos <br />
                 fim de semana
-              </div>
+                </div>
               ) : (
-                <div className="open-on-weekends dont-open">
-                <FiInfo size={32} color="#FF669D" />
+                  <div className="open-on-weekends dont-open">
+                    <FiInfo size={32} color="#FF669D" />
                 Não atendemos <br />
                 fim de semana
-              </div>
-              )}
+                  </div>
+                )}
             </div>
 
             {/* <button type="button" className="contact-button">
